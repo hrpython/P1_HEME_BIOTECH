b4834f4ad1a29dff309c41d940b29a9f8a29770d
hraza <hraza@192.168.0.30>
2021-07-01 15:08:38 +0200

    Ajout de commentaires JAVADOC file en parametre dans l'écriture dans le fichier

--- HemeBiotech/com/hemebiotech/analytics/RecordSymptomsToFile.java
+++ HemeBiotech/com/hemebiotech/analytics/RecordSymptomsToFile.java
@@ -8,26 +8,30 @@ import java.util.TreeMap;
 
 public class RecordSymptomsToFile {
 	TreeMap<String,Integer> tmIn = new TreeMap<String, Integer>();
+	
 	public RecordSymptomsToFile(TreeMap<String,Integer> tm) {
 		this.tmIn=tm;
 	}
-public void RecordToFile() {
-	
-	try {
-		FileWriter writer = new FileWriter ("result.out");
-		 for(Map.Entry<String,Integer> m:tmIn.entrySet()){    
-			 
-		      writer.write(m.getKey()+" "+m.getValue()+ "\n");  
-		      System.out.println(m.getKey()+" "+m.getValue());   
-		      } 
-		 writer.close();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-		    
-	}
-	
+	/**
+	 * Permet d'afficher et d'enregistrer les symptômes dans un fichier
+	 */
+	public void RecordToFile(String file) {
+		
+		try {
+			FileWriter writer = new FileWriter (file);
+			 for(Map.Entry<String,Integer> m:tmIn.entrySet()){    
+				 
+			      writer.write(m.getKey()+" "+m.getValue()+ "\n");  
+			      System.out.println(m.getKey()+" "+m.getValue());   
+			      } 
+			 writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			    
+		}
+		
 	
 	}
 	
